d4ae34dd4ec417308c93dc4f7a023c5132d18f92
OnyekaIgwe <oncigwe@gmail.com>
2022-04-27 21:40:56 -0400

    Changed locations of blockchain implementation
    Destructured the blockchain implementation from main.go file into a seperate package

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
 	"fmt"
+
+	"github.com/igweo/blockchain-go/blockchain"
 )
 
 type Block struct {
@@ -12,43 +12,14 @@ type Block struct {
 	PrevHash []byte
 }
 
-type BlockChain struct {
-	blocks []*Block
-}
-
-func (b *Block) ComputeHash() {
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-	hash := sha256.Sum256(info)
-	b.Hash = hash[:]
-
-}
-
-func CreateBlock(data string, prevHash []byte) *Block {
-	block := &Block{[]byte{}, []byte(data), prevHash}
-	block.ComputeHash()
-	return block
-}
-func (chain *BlockChain) AddBlock(data string) {
-	prevBlock := chain.blocks[len(chain.blocks)-1]
-	new := CreateBlock(data, prevBlock.Hash)
-	chain.blocks = append(chain.blocks, new)
-}
-
-func Genesis() *Block {
-	return CreateBlock("Genesis", []byte{})
-}
-
-func InitalizeBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{Genesis()}}
-}
 func main() {
-	chain := InitalizeBlockChain()
+	chain := blockchain.InitalizeBlockChain()
 
 	chain.AddBlock("First Block after Genesis")
 	chain.AddBlock("Second Block after Genesis")
 	chain.AddBlock("Third Block after Genesis")
 
-	for _, block := range chain.blocks {
+	for _, block := range chain.Blocks {
 		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
 		fmt.Printf("Data in Block: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
